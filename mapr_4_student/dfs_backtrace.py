dc4fd86fd549e8a5c157a4bceb473cc314a72450
macnack <maciej.p.krupka@student.put.poznan.pl>
2023-03-24 15:34:01 +0100

    add dfs

--- mapr_4_student/dfs_backtrace.py
+++ mapr_4_student/dfs_backtrace.py
@@ -1,5 +1,6 @@
 import rclpy
 import time
+import queue
 from mapr_4_student.grid_map import GridMap
 
 
@@ -24,8 +25,33 @@ class DFS(GridMap):
         #
         #
         ### YOUR CODE GOES ABOVE
-        pass  # DELETE THIS LINE IF THE CODE IS INSERTED
+        q = queue.LifoQueue()
+        visited = set()
+        q.put(self.start)
+        action = [(0,-1), (-1,0), (0,1), (1,0)]
+        while not q.empty():
+            #odczytaj
+            cur_n = q.queue[-1]
+            #zaznacz jako odwiedzony
+            visited.add(cur_n)
+            self.map.data[cur_n[0] + cur_n[1] * self.map.info.width] = 50
 
+            #Sprawdź, czy ten wierzchołek jest poszukiwanym wierzchołkiem końcowym
+            if cur_n == self.end:
+                break
+            else:
+                for u in action:
+                    next_n = (cur_n[0]+u[0], cur_n[1]+u[1])
+                    if (next_n not in visited) and self.map.data[next_n[0] + next_n[1] * self.map.info.width] < 50:
+                        q.put(next_n)
+                        break
+                    else:
+                        if( u == action[-1]):
+                            q.get()
+            
+            self.publish_visited()
+
+        self.publish_path(list(q.queue))
 
 def main(args=None):
     rclpy.init(args=args)
